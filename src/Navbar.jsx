32691045d4eda6e1c57b3470bf834473e548aedd
saurabh singh <saurabhsingh8043@gmail.com>
2022-10-29 14:00:46 +0530

    cart-icon added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import { AiOutlineShoppingCart } from "react-icons/fi";
+import { AiOutlineShoppingCart } from "react-icons/Ai";
 
 
 function Navbar(){
     return(
-        <div className="bg-gray-300">
-    <div className="w-1/3 ">
-    <img src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" alt="" />
-    {/* <div>AiOutlineShoppingCart</div> */}
-    </div>
+        <div className="flex justify-between bg-gray-300">
+    <div className="w-1/3">
+    <img src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" alt="" /></div>
+    <div className="m-12 text-4xl items-center	"><AiOutlineShoppingCart/></div>
+    
     </div>
     );
 
